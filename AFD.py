056a175b9d7e56ee5bdffcf7ef870bdb6b1b3c0a
Kaue Oliveira <kauelucas@terra.com.br>
2021-03-28 15:03:01 -0300

    Submit implemented

--- AFD.py
+++ AFD.py
@@ -1,4 +1,3 @@
-import sys
 import json
 
 def pertence(E, t):  # Verifica se o teste pertence aos simbolos de entrada
@@ -7,83 +6,55 @@ def pertence(E, t):  # Verifica se o teste pertence aos simbolos de entrada
     else:
         return 1
 
-def formatTransition(T):
-    convert = lambda x: "δ=("+str(x[0])+","+str(x[1])+")="+str(x[2])
-    aux = [convert(elem) for elem in F]
-    return aux
-
-def to_tuple(F):
-    aux = [tuple(elem) for elem in F]
-    return aux
-
-with open('ADFjson.json','r') as json_file:
-    ADF = json.load(json_file)
-# Parte dos dados
-E = ADF['E']  # Simbolos de entrada
-Q = ADF['Q']  # Lista de estados
-F = to_tuple(ADF['F'])  # Função de transição (Lista de tuplas) - [(Estado atual, simbolo, proximo estado),...]
-Q0 = ADF['Q0']  # Estado inicial
-QF = ADF['QF']  # Lista de estados finais
-C = input()  # Cadeia de teste
-
-class data:
+class data: #Objeto que guarda a execução do automato
     steps = []
     state = ""
 
-jsonData = data()
+def adf_start(E,Q,F,Q0,QF,C):
+    jsonData = data()
+    jsonData.steps = [] #Zera os passos do automato
+    Eatual = Q0  # Eatual representa o estado atual
+    i = 0  # i representa i indice da cadeia de teste
+    invalido = False
 
-for elem in formatTransition(F):
-    print(elem)
-
-#                      #
-#  INICIO DO AUTOMATO  #
-#                      #
-
-Eatual = Q0  # Eatual representa o estado atual
-i = 0  # i representa i indice da cadeia de teste
-
-while 1:
-    if pertence(E, C[i]) == 1:  # Verifica se a entrada atual pertence ao alfabeto
-        print("Erro, simbolo não existe no alfabeto!")
-        sys.exit()
-
-    # Procura alguma função com o mesmo estado que o atual e o mesmo simbolo que a cadeia
-    for index in range(0, len(F)):
-        erro = 1
-        if (F[index][0] == Eatual) and (F[index][1] == C[i]):
-            print(f"Lendo '{C[i]}' no estado '{Eatual}'")  # Passos
-            Eatual = F[index][2]  # proximo estado
-            print(
-                f"Função de Transição: ({F[index][0]},{F[index][1]}) = {F[index][2]}")  # Passos
-            i = i+1  # proximo elemento da cadeia de teste
-            erro = 0  # Controle para o loop
+    while 1:
+        if pertence(E, C[i]) == 1:  # Verifica se a entrada atual pertence ao alfabeto
+            jsonData.state = "REJEITADA, simbolo nao existe no alfabeto!"
+            invalido = True #Cadeia invalida
             break
 
-    if erro == 1:  # Verifica se existe nas funções a cadeia e o estado
-        print(
-            f"Erro, estado atual {Eatual} ou cadeia de teste {C[i]} não correspondida nas funções")
-        sys.exit()
+        # Procura alguma função com o mesmo estado que o atual e o mesmo simbolo que a cadeia
+        for index in range(0, len(F)):
+            erro = 1
+            if (F[index][0] == Eatual) and (F[index][1] == C[i]):
+                Eatual = F[index][2]  # proximo estado
+                jsonData.steps.append([F[index][0],F[index][1],F[index][2]]) # Guarda transicao executada
+                i = i+1  # proximo elemento da cadeia de teste
+                erro = 0  # Controle para o loop
+                break
 
-    if i == len(C):  # Verifica se toda a cadeia de teste ja passou
-        break
+        if erro == 1:  # Verifica se existe nas funções a cadeia e o estado
+            jsonData.state = f"REJEITADA, estado atual {Eatual} ou cadeia de teste {C[i]} nao correspondida nas funcoes"
+            invalido = True #Cadeia innvalida
+            break
 
-if Eatual in QF:  # Verifica se a cadeia foi aceitada ou não
-    print("ACEITA!")
-    data.state = "ACEITA!"
-else:
-    print("REJEITADA!")
-    jsonData.state = "REJEITADA!"
-    Efinal = str(QF)[1:-1]
-    print(f"O estado final foi o '{Eatual}' e não o {Efinal}")
+        if i == len(C):  # Verifica se toda a cadeia de teste ja passou
+            break
 
-jsonString = {
-    'steps':jsonData.steps,
-    'state':jsonData.state
-    }
+    if Eatual in QF and invalido is False:  # Verifica se a cadeia foi aceitada ou não
+        data.state = "ACEITA!" # Cadeia do automato aceita
+    elif invalido is False:
+        jsonData.state = "REJEITADA!" # Cadeia do automato rejeitada
+        Efinal = str(QF)[1:-1]
 
-print(jsonData.__dict__)
-with open("ADFresults.json","w") as write_file:
-    json.dump(jsonString,write_file)
+    jsonString = { # Formatacao do JSON de resultados
+        'steps':jsonData.steps,
+        'state':jsonData.state
+        }
+
+    with open("ADFresults.json","w") as write_file:
+        json.dump(jsonString,write_file)
+
 
 # Saida:
 # a quíntupla fornecida na entrada e para cadeia testada
--- AFDGUI.py
+++ AFDGUI.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 import json
+import AFD
 
 def to_tuple(f):
     aux = [tuple(elem) for elem in f]
@@ -11,19 +12,26 @@ def format_transition(t):
     return aux
 
 with open('ADFjson.json','r') as json_file:
-    ADF = json.load(json_file)
+    AFDjson = json.load(json_file)
 
-E = ADF['E']  # Simbolos de entrada
-Q = ADF['Q']  # Lista de estados
-F = to_tuple(ADF['F'])  # Função de transição (Lista de tuplas) - [(Estado atual, simbolo, proximo estado),...]
-Q0 = ADF['Q0']  # Estado inicial
-QF = ADF['QF']  # Lista de estados finais
+E = AFDjson['E']  # Simbolos de entrada
+Q = AFDjson['Q']  # Lista de estados
+F = to_tuple(AFDjson['F'])  # Função de transição (Lista de tuplas) - [(Estado atual, simbolo, proximo estado),...]
+Q0 = AFDjson['Q0']  # Estado inicial
+QF = AFDjson['QF']  # Lista de estados finais
+
 
 window = tk.Tk()
 label = tk.Label(text="Cadeia")
 entry = tk.Entry()
 Elabel = tk.Label(text="Σ={" + E + "}")
 Qlabel = tk.Label(text="Q={" + str(Q) + "}")
+
+def StartAFD(E,Q,F,Q0,QF):
+    C = entry.get()
+    AFD.adf_start(E,Q,F,Q0,QF,C)
+
+
 
 label.pack()
 entry.pack()
@@ -38,9 +46,10 @@ Flabel = tk.Label(text=str(format_transition(F)))
 Q0label = tk.Label(text="q0={" + str(Q0) + "}")
 QFlabel = tk.Label(text="F={" + str(QF) + "}")
 
-
+submit = tk.Button(window, text ="Submit", command = lambda: StartAFD(E,Q,F,Q0,QF))
 Q0label.pack()
 QFlabel.pack()
+submit.pack()
 
 # Enter into eventloop <- this will keep
 # running your application, until you exit

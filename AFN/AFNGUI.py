8f29599add5ff3689590134f27dc6d99309f8418
Guilherme de Castro Berti <50256830+GuilhermeBerti@users.noreply.github.com>
2021-04-13 18:23:29 -0300

    AFNGUI
    Interface do AFN

--- AFN/AFNGUI.py
+++ AFN/AFNGUI.py
@@ -1,8 +1,7 @@
 import tkinter as tk
 import json
-import AFD
-import AFDresultsGUI
-
+import AFN
+import AFNresultsGUI
 
 def to_tuple(f):
     aux = [tuple(elem) for elem in f]
@@ -13,14 +12,15 @@ def format_transition(t):
     aux = [convert(elem) for elem in t]
     return aux
 
-with open('AFNjson.json','r') as json_file:
-    AFDjson = json.load(json_file)
+with open('AFN/AFNjson.json','r') as json_file:
+    AFNjson = json.load(json_file)
 
-E = AFDjson['E']  # Simbolos de entrada
-Q = AFDjson['Q']  # Lista de estados
-F = to_tuple(AFDjson['F'])  # Função de transição (Lista de tuplas) - [(Estado atual, simbolo, proximo estado),...]
-Q0 = AFDjson['Q0']  # Estado inicial
-QF = AFDjson['QF']  # Lista de estados finais
+
+E = AFNjson['E']  # Simbolos de entrada
+Q = AFNjson['Q']  # Lista de estados
+F = to_tuple(AFNjson['F'])  # Função de transição (Lista de tuplas) - [(Estado atual, simbolo, proximo estado),...]
+Q0 = AFNjson['Q0']  # Estado inicial
+QF = AFNjson['QF']  # Lista de estados finais
 
 
 window = tk.Tk()
@@ -29,11 +29,36 @@ entry = tk.Entry()
 Elabel = tk.Label(text="Σ={" + E + "}")
 Qlabel = tk.Label(text="Q={" + str(Q) + "}")
 
+def StartAFD(E,Q,F,Q0,QF):
+    C = entry.get()
+    AFD.adf_start(E,Q,F,Q0,QF,C)
+    AFDresultsGUI.GUIstart(C)
+
 
 label.pack()
 entry.pack()
 Elabel.pack()
 Qlabel.pack()
+
+
+indice = 0
+scrollbar = tk.Scrollbar(window)
+scrollbar.pack( side = "right", fill = "y" )
+transicoes = format_transition(F)
+mylist = tk.Listbox(window, yscrollcommand = scrollbar.set )
+for elem in transicoes:
+     mylist.insert(indice,elem)
+     indice = indice + 1
+
+mylist.pack( side = "bottom", fill = "both" )
+scrollbar.config( command = mylist.yview )
+
+#Flabel = tk.Label(text=str(format_transition(F)))
+Q0label = tk.Label(text="q0={" + str(Q0) + "}")
+QFlabel = tk.Label(text="F={" + str(QF) + "}")
+#Flabel.pack()
+Q0label.pack()
+QFlabel.pack()
 
 
 submit = tk.Button(window, text ="Submit")

c05508c4be6e1cbb0a71d88aa7edb51734a56aab
Arthur Labaki <aplabaki@gmail.com>
2021-04-13 14:44:31 -0300

    Melhorando as funções de transição

--- AFN.py
+++ AFN.py
@@ -5,11 +5,11 @@ import sys
 E = '01'  # Simbolos de entrada
 Q = ['q0', 'q1', 'q2']  # Lista de estados
 # Função de transição (Lista de tuplas) - [(Estado atual, simbolo, proximo estado),...]  Funçao ε deve ser tratada como " " ex: ("q2", " ", "q3")
-F = [("q1", "0", "q1"), ("q1", "1", "q1"),
-     ("q1", "1", "q2"), ("q2", "0", "q3"), ("q2", "", "q3"), ("q3", "", "q4"), ("q4", "0", "q4"), ("q4", "1", "q4")]
-Q0 = 'q1'  # Estado inicial
-QF = ['q4']  # Lista de estados finais
-C = "1"  # Cadeia de teste
+F = [("q0", "0", ["q0"]), ("q0", "1", ["q0", "q1"]), ("q1", "0", ["q2"]),
+     ("q1", "", ["q2"]), ("q2", "1", ["q3"]), ("q2", "", ["q3"]), ("q3", "0", ["q3"]), ("q3", "1", ["q3"])]
+Q0 = 'q0'  # Estado inicial
+QF = ['q3']  # Lista de estados finais
+C = "0"  # Cadeia de teste
 
 
 #  INICIO DO AUTOMATO  #
@@ -36,8 +36,9 @@ def afnd_start(E, Q, F, Q0, QF, C):  # Função do automato inteiro
                 # Tratamento quando a C termina e ainda existe cadeia vazia possiveis na função
                 for var in range(0, len(F)):
                     if F[var][0] == Q0 and F[var][1] == '':
-                        if afnd_rec(E, Q, F, F[var][2], QF, C) == 0:
-                            return 0
+                        for est in range(0, len(F[var][2])):
+                            if afnd_rec(E, Q, F, F[var][2][est], QF, C) == 0:  # recursão
+                                return 0
                 return 1
 
         func = False
@@ -50,12 +51,14 @@ def afnd_start(E, Q, F, Q0, QF, C):  # Função do automato inteiro
             if F[index][0] == Q0:        # Encontra estado atual nas funções
                 if F[index][1] == C[0]:  # Encontra elemento da cadeia na função
                     func = True
-                    if afnd_rec(E, Q, F, F[index][2], QF, C[1:]) == 0:  # recursão
-                        return 0
+                    for est in range(0, len(F[index][2])):
+                        if afnd_rec(E, Q, F, F[index][2][est], QF, C[1:]) == 0:  # recursão
+                            return 0
                 if F[index][1] == '':   # Encontra cadeia vazia na função
                     func = True
-                    if afnd_rec(E, Q, F, F[index][2], QF, C) == 0:  # recursão
-                        return 0
+                    for est in range(0, len(F[index][2])):
+                        if afnd_rec(E, Q, F, F[index][2][est], QF, C) == 0:  # recursão
+                            return 0
 
         if func == False:
             return 1

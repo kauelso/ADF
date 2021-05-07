30687b865fd7db349b2375cf55a3195a75de9fa8
Kaue Oliveira <kauelucas@terra.com.br>
2021-05-07 16:30:29 -0300

    fixed converter

--- AFNE/infix_to_prefix.py
+++ AFNE/infix_to_prefix.py
@@ -19,7 +19,7 @@ def resolveLessOp(stack,elem,opcodes,output):
     if not stack:
         stack.append(elem)
 
-ex = "(a+(a.b))" #expressao de entrada
+ex = "0.1*+1" #expressao de entrada
 
 opcodes = ['+','.','*'] #operadores em ordem (menos relevante para o mais relevante)
 
@@ -36,27 +36,27 @@ for elem in exlist: #Para cada elemento da entrada
                 output.append(popped) #coloca o elemento retirado no output no output
                 resolveLessOp(stack,elem,opcodes,popped) #trata o elemento que ira ser colocado
             else:
-                resolveOp(stack,elem,opcodes,popped)
+                resolveOp(stack,elem,opcodes,popped) #Se o operador tiver maior precedencia ou o top da pilha nao for um operador ele é tratado nessa func
         else:
-            stack.append(elem)
-    elif elem == ')':
-        stack.append(elem)
-    elif elem == '(':
-        popped = stack.pop()
+            stack.append(elem) #se nao tiver nada na pilha, empilha o operador
+    elif elem == ')': #se  for um parenteses fechado
+        stack.append(elem) #empilha o parenteses
+    elif elem == '(':#se for um parenteses aberto
+        popped = stack.pop() #remove os elementos e coloca no output ate achar o parenteses fechado 
         output.append(popped)
         while popped != ')':
             popped = stack.pop()
             output.append(popped)
         output.pop()
-    else:
+    else: #caso nao seja nenhum outro caso ele sera um operador e deve ir para o output
         output.append(elem)
 
-if stack:
-    for elem in stack:
-        output.append(elem)
+while stack: #por fim, desempilha toda a pilha
+    elem = stack.pop()
+    output.append(elem)
 
-output.reverse()
-str1 = "".join(str(x) for x in output)
+output.reverse() #inverte o array de output
+str1 = "".join(str(x) for x in output) #transforma o array em uma string
 print(str1)
     
 
